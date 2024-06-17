e55e8a3a4bcf7fe8deac945c685be8e1a266060a
Дарья <zininadashuta167@gmail.com>
2024-06-17 15:32:02 +0300

    окно модуля

--- LearnSpas/Windows/LevelWindow.xaml.cs
+++ LearnSpas/Windows/LevelWindow.xaml.cs
@@ -23,5 +23,20 @@ namespace LearnSpas.Windows
         {
             InitializeComponent();
         }
+
+        private void LearnBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DopMaterialBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ProfilBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
