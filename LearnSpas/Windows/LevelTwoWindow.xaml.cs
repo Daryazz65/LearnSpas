26e9aed4a33dca1d017d3a11075eca1cc3810b6c
Дарья <zininadashuta167@gmail.com>
2024-06-18 16:06:14 +0300

    Окно второго модуля. Уровни

---
+++ LearnSpas/Windows/LevelTwoWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LearnSpas.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для LevelTwoWindow.xaml
+    /// </summary>
+    public partial class LevelTwoWindow : Window
+    {
+        public LevelTwoWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void VozvratModulBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LearnBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DopMaterialBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ProfilBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LevelOneBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LevelTwoBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LevelThreeBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
